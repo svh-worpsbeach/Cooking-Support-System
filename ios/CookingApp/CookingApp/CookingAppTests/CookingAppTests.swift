f5e9687d5d157001160017e81dca669bea4bbb26
svh-worpsbeach <sven@worpsbeach.de>
2026-04-14 14:55:53 +0200

    Comment ...

---
+++ ios/CookingApp/CookingApp/CookingAppTests/CookingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CookingAppTests.swift
+//  CookingAppTests
+//
+//  Created by Sven Hapke on 14.04.26.
+//
+
+import Testing
+@testable import CookingApp
+
+struct CookingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
